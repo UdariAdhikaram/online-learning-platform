d72b2539ed4cc3735998e7443b89f3db3905f625
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-11-02 01:08:02 +0530

    create Enrollment.js for Define a schema to link students with courses.

---
+++ backend/models/Enrollment.js
@@ -0,0 +1,8 @@
+const enrollmentSchema = new mongoose.Schema({
+    student: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+    enrollmentDate: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model('Enrollment', enrollmentSchema);
+  
