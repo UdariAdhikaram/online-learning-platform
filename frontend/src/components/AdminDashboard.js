09d31dd886aa69a48a9d301c1a1a1fbcdfbb6afc
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-11-02 13:57:04 +0530

    Change the admin dashboard content

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,6 +1,6 @@
-// src/components/AdminDashboard.js
 import React, { useEffect, useState } from 'react';
 import { getCoursesForAdmin } from '../api';
+import './AdminDashboard.css';
 
 const AdminDashboard = () => {
   const [courses, setCourses] = useState([]);
@@ -19,19 +19,22 @@ const AdminDashboard = () => {
   }, [token]);
 
   return (
-    <div>
-      <h2>Admin Dashboard</h2>
-      <h3>Manage Courses</h3>
-      <ul>
-        {courses.map(course => (
-          <li key={course._id}>
-            <h4>{course.title}</h4>
-            <p>{course.description}</p>
-            {/* Add buttons for Update/Delete functionality */}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div className='body'>
+    <div className="admin_dashboard_container">
+  <h2>Admin Dashboard</h2>
+  <h3>Manage Courses</h3>
+  <ul>
+    {courses.map((course) => (
+      <li key={course._id}>
+        <h4>{course.title}</h4>
+        <p>{course.description}</p>
+        <button className="update_button">Update</button>
+        <button className="delete_button">Delete</button>
+      </li>
+    ))}
+  </ul>
+</div>
+</div>
   );
 };
 
