6b413862968f31dc576e352708cb8bd421e73d53
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-11-04 11:46:47 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //unblock cross origin resource sharing
+app.use(express.json()); // request access sharing using the json format
 
 // Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
